5ba593dd00cfbf2af54c6470753ccb9ae470dbc7
Onfephost <onfephost@gmail.com>
2026-06-29 09:41:55 +0000

    refixed

--- Python/1.0.0(Python)/onfex.py
+++ Python/1.0.0(Python)/onfex.py
@@ -1,10 +1,5 @@
 
-import sys
 import os
-from cache.Exceptions import *
-from cache.lexer import lex
-from cache.parser import Parser
-from cache.interpreter import Interpreter
 from cache.document import OnfexPloneDoph as OPD
 
 main_onfex = OPD(os.path.dirname(os.path.abspath(__file__)),"main.onfex",None,False)
