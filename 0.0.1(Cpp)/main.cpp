04aa53a4aa0952755df821d57b2906e34c536f53
Onfephost <onfephost@gmail.com>
2026-06-29 16:15:22 +0000

    reforged

--- 0.0.1(Cpp)/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "onfex.hpp"
-
-int main(int argc, char** argv) {
-    std::string path = (argc > 1) ? argv[1] : "main.onfex";
-    onfex::run_file(path);
-    return 0;
-}
--- 1.0.0(Rust)/src/runtime.rs
+++
@@ -1,7 +0,0 @@
-use crate::ast::Type;
-#[derive(Debug)]
-pub enum RuntimeSignal{
-    Return(Type),
-    Error(String),
-}
-pub type OnfexResult=Result<Type,RuntimeSignal>;
