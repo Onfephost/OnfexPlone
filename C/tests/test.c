f565ff658e76767df5fae7cb13978b70d203e934
Onfephost <onfephost@gmail.com>
2026-07-02 19:18:32 +0000

    c tests...

--- C/tests/test.c
+++ C/tests/test.c
@@ -2,35 +2,15 @@
 #include <string.h>
 
 int main() {
-    char username[20];
-    char password[20];
-    const char *correct_user = "admin";
-    const char *correct_pass = "1234";
-    int attempts = 3;
-    
-    printf("=== Login ===\n\n");
-    
-    while (attempts > 0) {
-        printf("Username: ");
-        fgets(username, sizeof(username), stdin);
-        username[strcspn(username, "\n")] = 0;
-        
-        printf("Password: ");
-        fgets(password, sizeof(password), stdin);
-        password[strcspn(password, "\n")] = 0;
-        
-        if (strcmp(username, correct_user) == 0 && strcmp(password, correct_pass) == 0) {
-            printf("\n✓ Logined up,Wellcome %s\n", username);
-            return 0;
-        } else {
-            attempts--;
-            if (attempts > 0) {
-                printf("✗ Wrong username or password! Remaining attempts: %d\n\n", attempts);
-            } else {
-                printf("✗ Account locked! Too many failed attempts.\n");
-            }
-        }
+    int nots[4];
+    for (int i = 0; i < 4; i++) {
+        printf("Enter %d. note: ", i + 1);
+        scanf("%d", &nots[i]);
     }
-    
-    return 1;
+    int sum = 0;
+    for (int i = 0; i < 4; i++) {
+        sum += nots[i];
+    }
+    printf("Average of notes: %d\n", sum/4);
+    return 0;
 }
